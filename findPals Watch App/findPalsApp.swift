15cfe08970413fe40917d8a94df75d8bbb4ccb25
bernanda-rai <inotbnri@gmail.com>
2024-05-23 09:00:24 +0700

    first init

---
+++ findPals Watch App/findPalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  findPalsApp.swift
+//  findPals Watch App
+//
+//  Created by mac.bernanda on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct findPals_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
